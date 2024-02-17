9f6f081bc622c984e731a802cf7e8a54c8ceab66
Maria Saavedra <msaav3@gmail.com>
2024-02-16 20:50:19 -0600

    chore: wip

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,31 @@
-import "./styles.css";
-import { setupCounter } from "./counter.ts";
+const slides = document.querySelectorAll<HTMLDivElement>(".accordion__item");
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <div class="card">
-    <button id="counter" type="button"></button>
-    </div>
-  </div>
-`;
+const toggleSlide = (slide: HTMLDivElement) => {
+  assignActiveCard(slide);
+};
+const assignActiveCard = (slide: HTMLDivElement) => {
+  const index = slide.dataset.index;
+  switch (index) {
+    case "0":
+      slides[0]?.classList.add("active");
+      slides[1]?.classList.remove("active");
+      slides[2]?.classList.remove("active");
+      break;
+    case "1":
+      slides[0]?.classList.remove("active");
+      slides[1]?.classList.add("active");
+      slides[2]?.classList.remove("active");
+      break;
+    case "2":
+      slides[0]?.classList.add("active");
+      slides[1]?.classList.add("active");
+      slides[2]?.classList.add("active");
+      break;
+    default:
+      console.log("default");
+  }
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+for (const slide of slides) {
+  slide.addEventListener("click", () => toggleSlide(slide));
+}
